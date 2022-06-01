6f3b18a109545add6afc547243d8cdbf1e53e1be
Victor Ghita <V.F.Ghita@student.tudelft.nl>
2022-06-01 11:59:08 +0200

    added dark theme and changed the layout for tablets

---
+++ theme.js
@@ -0,0 +1,24 @@
+var toggle = document.getElementById("theme-toggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme) {
+    document.documentElement.setAttribute('data-theme', storedTheme)
+    toggle.checked = true;
+}
+
+var currentTheme = document.documentElement.getAttribute("data-theme");
+if (currentTheme === 'light') toggle.checked = false;
+
+
+toggle.onclick = function() {
+    // console.log("Clicked!");
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};

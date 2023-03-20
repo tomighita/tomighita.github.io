b76a40a6287076f80af6c7da86a077ff58937d06
Victor Ghita <tomi.gvf@gmail.com>
2023-03-20 13:18:57 +0100

    Setting up V2

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					"primary": "#F28C18",	 
+					"secondary": "#22d3ee",
+					"accent": "#a21caf",
+					"neutral": "#1B1D1D",
+					"base-100": "#212121",
+					"info": "#818cf8",
+					"success": "#84cc16",
+					"warning": "#facc15",
+					"error": "#DC2626",
+				}
+			}
+		],
+	},
+}
